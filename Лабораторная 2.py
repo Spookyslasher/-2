6cd499a0d89395407f7758aa852f5ef8e4931817
BorysenkoMax <90186041+Spookyslasher@users.noreply.github.com>
2021-09-14 14:34:59 +0300

    Add files via upload

---
+++ Лабораторная 2.py
@@ -0,0 +1,17 @@
+while True:
+    s=input("Знак (+,-,/,*): ")
+    a=float(input('a= '))
+    b=float(input('b= '))
+    if s==('+'):
+        print(a+b)
+    elif s==('-'):
+        print(a-b)
+    elif s==('*'):
+            print(a*b)
+    elif s==('/'):
+        if b !=0:
+            print (a/b)
+else:print("Ділення на нуль")
+ 
+                    
+
